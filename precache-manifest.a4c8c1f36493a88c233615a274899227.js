9c5994fc17eeb21f9096bf2311a7e2b70f0a9b07
ismail <slowayni@.com>
2020-03-02 22:55:51 +0100

    Updates

---
+++ precache-manifest.a4c8c1f36493a88c233615a274899227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179a5908396e886dae0e81ecc7c38972",
+    "url": "/todolist-demo/index.html"
+  },
+  {
+    "revision": "5d2eb36610950688a5ab",
+    "url": "/todolist-demo/static/css/main.6aa38be6.chunk.css"
+  },
+  {
+    "revision": "04bc2aff9b573dcec6ea",
+    "url": "/todolist-demo/static/js/2.cdb20b5f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todolist-demo/static/js/2.cdb20b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2eb36610950688a5ab",
+    "url": "/todolist-demo/static/js/main.ae3967aa.chunk.js"
+  },
+  {
+    "revision": "e55eb9b85a39557bde21",
+    "url": "/todolist-demo/static/js/runtime-main.c8d63d32.js"
+  }
+]);
